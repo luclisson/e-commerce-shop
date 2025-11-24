631b7a5ab20036c2a04968d2cb3813a18218215e
Luc <lisson.luc@gmail.com>
2025-11-24 12:49:46 +0100

    project init (spring + react)

---
+++ backend/shop/src/main/java/com/ecom/shop/NichtDieBohneApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NichtDieBohneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NichtDieBohneApplication.class, args);
+	}
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+        <h1 class="text-2xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
